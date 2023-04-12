b0b113d93c6aacf77aad9dacec63518bfb29838e
Fool4T <Fool4T@MacBook-Pro-4.local>
2023-04-12 13:09:38 +0800

    singers app

--- src/api/request.js
+++ src/api/request.js
@@ -7,3 +7,14 @@ export const getBannerRequest = () => {
 export const getRecommendListRequest = () => {
   return axiosInstance.get("/personalized");
 };
+
+export const getHotSingerListRequest = (count) => {
+  return axiosInstance.get(`/top/artists?offset=${count * 30}&limit=30`);
+};
+
+export const getSingerListRequest = (type, area, alpha, offset) => {
+  const al = alpha.toLowerCase();
+  return axiosInstance.get(
+    `/artist/list?type=${type}&area=${area}&initial=${al}&offset=${offset * 30}`
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import recommendSlice from "../application/Recommend/store/slice";
+import singersSlice from "../application/Singers/store/slice";
 //全局store
 const store = configureStore({
   reducer: {
     recommendReducer: recommendSlice,
+    singersReducer: singersSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
